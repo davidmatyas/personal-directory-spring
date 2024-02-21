7b8ce7f133ce0f5043124c983f22a02886e4dea5
David <david.matyas.cz@gmail.com>
2024-02-21 05:48:07 +0100

    initial commit

---
+++ src/main/java/com/personal/personaldirectoryspring/PersonalDirectorySpringApplication.java
@@ -0,0 +1,13 @@
+package com.personal.personaldirectoryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDirectorySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDirectorySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/personaldirectoryspring/PersonalDirectorySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.personaldirectoryspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDirectorySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
